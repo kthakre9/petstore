7fbb2e002786d5a74338e8fdefbd54c4d98c1274
Ketki Thakre <kthakre@us.ibm.com>
2017-03-04 18:32:44 -0500

    list view updated

--- app/components/DogsList.js
+++ app/components/DogsList.js
@@ -4,91 +4,171 @@ export default React.createClass({
     render() {
         return (
             <div className="lists">
+                <h1>Dogs</h1>
                 <div className="row">
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="grid">
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
+                    </div>
+                    <div className="grid">
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
+                    </div>
+                    <div className="grid">
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
+                    </div>
+                    <div className="grid">
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
+                    </div>
+                    <div className="grid">
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DMH Black, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Carolina</h2>
+                        <p>Brown Tabby Tuxedo, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Ellsa</h2>
+                        <p>Dijon – DSH Orange Tabby, Male</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Aslyn</h2>
+                        <p>DSH, Dilute Calico, Female</p>
                     </div>
                     <div className="grid">
-                        <h2>Dog shop </h2>
+                        <img src="./images/dog.jpg" width={250} height={200} />
+                        <h2>Nahla</h2>
+                        <p>DMaine Coon mix, Female</p>
                     </div>
                 </div>
+
             </div>
         );
     }
