e6d5a328558e422c71e520d806eac4b36abf5225
Ketki Thakre <thakre.ketki@gmail.com>
2017-04-24 14:48:37 -0400

    upgrade to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     context: path.join(__dirname, "/app"),
-    entry: "./app.js",
-    devtool: "source-map",
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8001/#/',
+        'webpack/hot/only-dev-server',
+        "./app.js"
+    ],
+    devtool: "inline-source-map",
     output: {
         path: __dirname,
         publicPath: "/build/",
         filename: "app.js"
     },
     devServer: {
+        hot: true,
+        inline: true,
+        historyApiFallback: true,
         contentBase: "app",
         port: process.env.PORT || 8001,
         proxy: {
@@ -21,31 +30,47 @@ module.exports = {
         }
     },
     resolve: {
-        modulesDirectories: ["web_modules","node_modules"],
-        extensions: ["", ".js", ".json", ".es6"]
+        modules: [
+            path.join(__dirname),
+            "node_modules"
+        ],
+        extensions: [".js", ".json", ".es6"]
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(es6|js)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
+                exclude: [
+                    path.resolve(__dirname, "/node_modules/")
+                ],
+                use: [
+                    "babel-loader"
+                ]
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader",
+                        "sass-loader"
+                    ],
+                })
             },
             {
                 test: /\.(jpg|png|svg|ttf|eot|woff|woff2)$/,
-                loader: "file?name=public/fonts/[name].[ext]"
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
+                use: [
+                    "file?name=public/fonts/[name].[ext]"
+                ]
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin("style.css")
-    ]
+        new ExtractTextPlugin("style.css"),
+        new webpack.HotModuleReplacementPlugin(),
+        // enable HMR globally
+
+        new webpack.NamedModulesPlugin(),
+        // prints more readable module names in the browser console on HMR updates
+    ],
 };
